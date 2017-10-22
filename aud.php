e08eac52470a5a634dd964271ee5c4ac1f6de2fb
unknown <jesuyed@yahoo.com>
2017-10-22 02:34:54 -0400

    Oh the new ones

---
+++ aud.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/aud.css">
+    <script src="js/aud.js"></script>
+</head>
+
+<body>
+    <div id="inviteID">
+        Invite ID: <input type="text" name="inviteID">
+        <br><br>   
+        <text id="demo"></text>
+        <input type="submit"  value="Enter">
+    </div>
+
+    <div id="djSongs">
+        Here is your DJ's playlist:
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+
+<body>  
+    <div id="heading">
+        <h2>WELCOME TO EVERYDJA!</h2>
+        <a href="djay.php">DJ Area</a><br>
+        <a href="aud.php">Audience Area</a>
+    </div>
+
+</body>
+</html>
