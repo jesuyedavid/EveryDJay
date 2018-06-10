88f564943a196d45689d6ec52416422ea615b65f
tabularrasa <jesuyed@yahoo.com>
2018-06-10 00:44:40 -0700

    Trying to show data from api db to php

--- everdjayapp/djay.php
+++ everdjayapp/djay.php
@@ -5,53 +5,12 @@
         <link rel="stylesheet" href="css/dj.css">
         <link rel="shortcut icon" href="pics/favicon.jpg">
         <script src="js/dj.js"></script>
-
-        <?php
-            $connection=mysqli_connect('db', 'root', 'test', 'myDb');
-            if (!$connection){
-                echo "Not Connected";
-            }
-            $DisplayForm=True;
-            if(isset($_POST['djID'])){
-                $DisplayForm=False;
-            };
-            $iDdata = $_POST['djID'];
-            $uNdata=$_POST['djUsername'];
-            $sql="SELECT * FROM alldjays WHERE djNo = '".$iDdata."'";
-            $records=mysqli_query($connection, $sql);
-        ?>
-
     </head>
 
     <body>
-        <?php
-            if (mysqli_num_rows($records)>0){
-                while ($eachrow=mysqli_fetch_array($records)){
-                    echo "<tr>";
-                    echo "<td>".$eachrow['djNo']."</td>";
-                    echo "<td>".$eachrow['firstName']."</td>";
-                    echo "<td>".$eachrow['lastName']."</td>";
-                    echo "<td>".$eachrow['userName']."</td>";             
-                    echo "<tr>";
-                }
-            }else{
-                echo "Invalid Invite ID";
-            }
-        if ($DisplayForm){
-        ?>
-            <div id="djIDform">
-                <form id="inviteID" action="" method="POST">
-                    DJ ID: <input type="text" name="djID"> <br>
-                    DJ Username: <input type="text" name="djUsername">
-                    <br><br>   
-                    <text id="demo"></text>
-                    <input type="submit" name="getdjID" id="getdjID" value="Enter">
-                </form>
-            </div>
-        <?php
-        } 
-        ?>
-        
+        <div id="answers">
+
+        </div>
         <div id="djSongs">
             <a>Spotify</a><br>
             <a>Youtube</a><br>
--- everdjayapp/js/dj.js
+++ everdjayapp/js/dj.js
@@ -1,3 +1,36 @@
-$(document).ready(function(){
-   
-});
+responseField = document.getElementById('answers');
+
+const renderResponse = (res) => {
+    console.log("we here");
+    // Displays either message depending on results
+    if(res.errors){
+        document.getElementById('answers').innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+    } else {  
+        document.getElementById('answers').innerHTML = `<p>DJays are: </p><p> ${res[0]['Location']} </p>`;
+    }
+}
+
+  
+const getSuggestions = async()=>{    
+    const endpoint='https://everydjay-a275.restdb.io/rest/c-2-import-cal-event-sample';
+    //const endpoint='https://api.datamuse.com/words?rel_jja=table'
+    try{
+      const response=await fetch(endpoint,{
+        method:'GET',
+        headers:{
+                    'Content-type': 'application/json',
+                    'apikey': "5b1b959146624c7b24444d69"
+                }
+      });
+      if(response.ok){
+        const jsonResponse=await response.json();
+        renderResponse(jsonResponse);
+      }
+    }
+    catch(error){
+      console.log(error);
+    }
+}
+
+getSuggestions()
+
