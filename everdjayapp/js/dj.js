855bd1b87ccb678c7671557879ae01a0a02af59f
tabularrasa <jesuyed@yahoo.com>
2018-06-11 00:41:47 -0700

    dj can now add event, remaining audience

--- everdjayapp/js/dj.js
+++ everdjayapp/js/dj.js
@@ -6,25 +6,43 @@ const renderResponse = (res) => {
     }
 }
 
+const generateID=(djname, djpurpose, djdate, djtime)=>{
+    return (djdate+djname+djpurpose+djtime).replace(/\W/g, '')
+}
   
-const getSuggestions = async()=>{    
-    const endpoint='https://everydjay-a275.restdb.io/rest/c-2-import-cal-event-sample';
+const accessDB = async()=>{ 
+    var djname=document.getElementById("djname").value;
+    var djpurpose=document.getElementById("djpurpose").value;
+    var djdate=document.getElementById("djdate").value;
+    var djtime=document.getElementById("djtime").value
+
+    var data = JSON.stringify({
+        "evenID": generateID(djname, djpurpose, djdate, djtime),
+        "Name": djname,
+        "Purpose":djpurpose,
+        "Date":djdate,
+        "Time":djtime
+      });
+
+    const endpoint='https://everydjay-a275.restdb.io/rest/eventfromdj';
     try{
       const response=await fetch(endpoint,{
-        method:'GET',
+        method:'POST',
+        body: data,
         headers:{
                     'Content-type': 'application/json',
-                    'apikey': "5b1b959146624c7b24444d69"
+                    'apikey': "5b1e107f46624c7b24444db4"
                 }
       });
       if(response.ok){
         const jsonResponse=await response.json();
-        renderResponse(jsonResponse);
+        //console.log(jsonResponse);
       }
     }
     catch(error){
       console.log(error);
     }
+    createDone();
 }
 
 creaEveFuncC=()=>{
@@ -32,17 +50,20 @@ creaEveFuncC=()=>{
 }
 
 createDone=()=>{
+    document.getElementById("djname").value="";
+    document.getElementById("djpurpose").value="";
+    document.getElementById("djdate").value="";
+    document.getElementById("djtime").value="";    
     document.getElementById('evelist').style.display="none";
 }
 
 //to run our predefined functions
-
+//after page has loaded
 $(document).ready(function() {
     var el = document.getElementById("creaEveFunc");
     el.addEventListener('click', creaEveFuncC);
 
-    var doneIn = document.getElementById("doneIn");
-    doneIn.addEventListener('click', createDone);
+    var eventfromdj = document.getElementById("doneIn");
+    eventfromdj.addEventListener('click', accessDB);
 
-    getSuggestions()
 });
